9a8bf38218b99b8bb9247f5787a03bdd5f284d82
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-31 16:18:47 +1200

    Green window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
-using namespace std;
-int main()
+
+int main(void)
 {
-  cout << "Hello World!";
-  SDL_Init(SDL_INIT_VIDEO);
-  return 0;
+  SDL_Window *win = NULL;
+  SDL_Renderer *ren = NULL;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_CreateWindowAndRenderer(320, 640, 0, &win, &ren);
+
+  SDL_SetRenderDrawColor(ren, 0, 255, 0, 255);
+  SDL_RenderClear(ren);
+  SDL_RenderPresent(ren);
+  bool quit = false;
+
+  // Event handler
+  SDL_Event e;
+
+  // While application is running
+  while (!quit)
+  {
+    // Handle events on queue
+    while (SDL_PollEvent(&e) != 0) // poll for event
+    {
+      // User requests quit
+      if (e.type == SDL_QUIT) // unless player manually quits
+      {
+        quit = true;
+      }
+    }
+  }
+
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+
+  return (0);
 }
