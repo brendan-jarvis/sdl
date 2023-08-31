a094033534b29960bf464b8b7fa3130dfda63008
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-31 16:32:27 +1200

    Can change color and quit

--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@ int main(void)
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_CreateWindowAndRenderer(480, 320, 0, &window, &renderer);
 
-  SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
   bool quit = false;
 
   // Event handler
   SDL_Event e;
+
+  // Color variables
+  int r = 255;
+  int g = 0;
+  int b = 0;
 
   // While application is running
   while (!quit)
@@ -28,6 +30,52 @@ int main(void)
       {
         quit = true;
       }
+    }
+
+    // Delay to reduce CPU usage
+    SDL_Delay(10);
+
+    // Check for key press
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_ESCAPE])
+    {
+      quit = true;
+    }
+
+    // Reset color to red when user presses 'r'
+    if (state[SDL_SCANCODE_R])
+    {
+      r = 255;
+      g = 0;
+      b = 0;
+
+      SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear(renderer);
+      SDL_RenderPresent(renderer);
+    }
+
+    // Reset color to green when user presses 'g'
+    if (state[SDL_SCANCODE_G])
+    {
+      r = 0;
+      g = 255;
+      b = 0;
+
+      SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear(renderer);
+      SDL_RenderPresent(renderer);
+    }
+
+    // Reset color to blue when user presses 'b'
+    if (state[SDL_SCANCODE_B])
+    {
+      r = 0;
+      g = 0;
+      b = 255;
+
+      SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear(renderer);
+      SDL_RenderPresent(renderer);
     }
   }
 
