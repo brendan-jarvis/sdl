57c39f74c63818a51cf0cde4bdb6d17f4093282f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-09-01 16:39:29 +1200

    setup Player

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 // #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+// Structs/Classes
+class Player
+{
+public:
+  int x, y, width, height, velX, velY, speed, lives, score;
+  bool is_alive;
+};
+
 // Constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -15,6 +23,26 @@ int game_is_running = false;
 int last_frame_time = 0;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+Player player;
+
+void setup(void)
+{
+  // initialise the star background
+
+  // initialise the player
+  player.x = 20;
+  player.y = 20;
+  player.width = 20;
+  player.height = 20;
+  player.velX = 0;
+  player.velY = 0;
+  player.speed = 5;
+  player.is_alive = true;
+  player.score = 0;
+  player.lives = 3;
+
+  // initialise the asteroids
+}
 
 bool initialiseWindow(void)
 {
@@ -48,6 +76,8 @@ void renderOutput(void) {}
 int main(void)
 {
   game_is_running = initialiseWindow();
+
+  setup();
 
   while (game_is_running)
   {
