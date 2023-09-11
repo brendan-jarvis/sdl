9629bbd36e7b4827ea5658a29ca3a5a5d4117479
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-09-12 00:22:32 +1200

    Remove default width, height from Brick class

--- Breakout/src/main.cpp
+++ Breakout/src/main.cpp
@@ -1,26 +1,29 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL_ttf.h>
 #include <iostream>
 
 // Structs/Classes
-class Ball {
+class Ball
+{
 public:
   float x, y, direction;
   int width, height, lives, score, speed, acceleration;
   bool is_alive;
 };
 
-class Player {
+class Player
+{
 public:
   float x, y;
   int width, height, lives, score, speed, acceleration;
   bool is_alive;
 };
 
-class Brick {
+class Brick
+{
 public:
   float x, y;
-  int width = 60, height = 20;
+  int width, height;
   bool is_alive = true;
 };
 
@@ -39,7 +42,8 @@ Brick bricks[24];
 SDL_Event event;
 TTF_Font *font;
 
-void setup(void) {
+void setup(void)
+{
   // initialise the player
   player.x =
       (SCREEN_WIDTH / 2.0) - (player.width / 2.0); // middle of the screen
@@ -64,26 +68,34 @@ void setup(void) {
 
   // initialise 24 bricks on the screen
   // TODO: change this pattern of bricks
-  for (int i = 0; i < 24; i++) {
+  for (int i = 0; i < 24; i++)
+  {
+    bricks[i].width = 60;
+    bricks[i].height = 20;
     bricks[i].x = (SCREEN_WIDTH / 2.0) - (bricks[i].width * 3.0) - 10.0;
     bricks[i].y = 50;
-    if (i > 5) {
+    if (i > 5)
+    {
       bricks[i].x += (bricks[i].width + 10) * (i % 6);
       bricks[i].y += (bricks[i].height + 10) * 1;
     }
-    if (i > 11) {
+    if (i > 11)
+    {
       bricks[i].x += (bricks[i].width + 10) * (i % 6);
       bricks[i].y += (bricks[i].height + 10) * 1;
     }
-    if (i > 17) {
+    if (i > 17)
+    {
       bricks[i].x += (bricks[i].width + 10) * (i % 6);
       bricks[i].y += (bricks[i].height + 10) * 1;
     }
   }
 }
 
-bool initialiseWindow(void) {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+bool initialiseWindow(void)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
     std::cout << "SDL could not initialise! SDL_Error: " << SDL_GetError()
               << std::endl;
     return false;
@@ -92,27 +104,33 @@ bool initialiseWindow(void) {
                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-  if (!window) {
+  if (!window)
+  {
     std::cout << "SDL could not create window! SDL_Error: " << SDL_GetError()
               << std::endl;
   }
   renderer = SDL_CreateRenderer(window, -1, 0);
 
-  if (!renderer) {
+  if (!renderer)
+  {
     std::cout << "SDL could not create renderer! SDL_Error: " << SDL_GetError()
               << std::endl;
   }
 
-  if (TTF_Init() == -1) {
+  if (TTF_Init() == -1)
+  {
     std::cout << "Could not initailize SDL2_ttf, error: " << TTF_GetError()
               << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "SDL2_ttf system ready to go!" << std::endl;
   }
 
   font = TTF_OpenFont("assets/fonts/Asteroids.ttf", 32);
 
-  if (font == nullptr) {
+  if (font == nullptr)
+  {
     std::cout << "Could not load font" << std::endl;
     return false;
   }
@@ -120,14 +138,18 @@ bool initialiseWindow(void) {
   return true;
 }
 
-void processInput(void) {
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
+void processInput(void)
+{
+  while (SDL_PollEvent(&event))
+  {
+    switch (event.type)
+    {
     case SDL_QUIT:
       game_is_running = false;
       break;
     case SDL_KEYDOWN:
-      switch (event.key.keysym.sym) {
+      switch (event.key.keysym.sym)
+      {
       case SDLK_ESCAPE:
         game_is_running = false;
         break;
@@ -157,7 +179,8 @@ void processInput(void) {
   }
 }
 
-void updateGame(void) {
+void updateGame(void)
+{
   float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
 
   last_frame_time = SDL_GetTicks();
@@ -166,7 +189,8 @@ void updateGame(void) {
   player.x += player.acceleration * delta_time;
 
   // speed up the ball after setup
-  if (ball.speed < 100) {
+  if (ball.speed < 100)
+  {
     ball.speed += 1;
   }
 
@@ -175,49 +199,65 @@ void updateGame(void) {
   ball.y += ball.speed * sin(ball.direction) * delta_time;
 
   // Check for player collision with the edge of the screen
-  if (player.x <= 0) {
+  if (player.x <= 0)
+  {
     player.x = 0;
     player.acceleration = 0;
-  } else if (player.x >= SCREEN_WIDTH - player.width) {
+  }
+  else if (player.x >= SCREEN_WIDTH - player.width)
+  {
     player.x = SCREEN_WIDTH - player.width;
     player.acceleration = 0;
   }
 
   // Check for ball collision with the edge of the screen
-  if (ball.x <= 0) {
+  if (ball.x <= 0)
+  {
     ball.x = 0;
     ball.direction = M_PI - ball.direction;
-  } else if (ball.x >= SCREEN_WIDTH - ball.width) {
+  }
+  else if (ball.x >= SCREEN_WIDTH - ball.width)
+  {
     ball.x = SCREEN_WIDTH - ball.width;
     ball.direction = M_PI - ball.direction;
-  } else if (ball.y <= 0) {
+  }
+  else if (ball.y <= 0)
+  {
     ball.y = 0;
     ball.direction = -ball.direction;
-  } else if (ball.y >= SCREEN_HEIGHT - ball.height) {
+  }
+  else if (ball.y >= SCREEN_HEIGHT - ball.height)
+  {
     player.lives--;
     ball.x = (SCREEN_WIDTH / 2.0) - (ball.width / 2.0); // middle of the screen
     ball.y =
-        (SCREEN_HEIGHT / 2.0) - (ball.height / 2.0); // middle of the screen
+        (SCREEN_HEIGHT / 2.0) - (ball.height / 2.0);      // middle of the screen
     ball.direction = (float)rand() / RAND_MAX * 2 * M_PI; // random direction
     ball.speed = 100;
   }
 
   // check for ball collision with the player
   // prevent the ball overlapping the player
-  if (ball.y + ball.height >= player.y && ball.y <= player.y + player.height) {
-    if (ball.x + ball.width >= player.x && ball.x <= player.x + player.width) {
+  if (ball.y + ball.height >= player.y && ball.y <= player.y + player.height)
+  {
+    if (ball.x + ball.width >= player.x && ball.x <= player.x + player.width)
+    {
       ball.y = player.y - ball.height;
       ball.direction = -ball.direction;
     }
   }
 
   // check for ball collision with the bricks
-  for (int i = 0; i < 24; i++) {
-    if (bricks[i].is_alive) {
+  for (int i = 0; i < 24; i++)
+  {
+    if (bricks[i].is_alive)
+    {
       if (ball.y + ball.height >= bricks[i].y &&
-          ball.y <= bricks[i].y + bricks[i].height) {
+          ball.y <= bricks[i].y + bricks[i].height)
+      {
         if (ball.x + ball.width >= bricks[i].x &&
-            ball.x <= bricks[i].x + bricks[i].width) {
+            ball.x <= bricks[i].x + bricks[i].width)
+        {
           bricks[i].is_alive = false;
           ball.direction = -ball.direction;
           player.score++;
@@ -227,7 +267,8 @@ void updateGame(void) {
   }
 }
 
-void renderOutput(void) {
+void renderOutput(void)
+{
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
@@ -237,8 +278,10 @@ void renderOutput(void) {
   SDL_Rect ballRect = {(int)ball.x, (int)ball.y, ball.width, ball.height};
 
   // draw for the bricks
-  for (int i = 0; i < 24; i++) {
-    if (bricks[i].is_alive) {
+  for (int i = 0; i < 24; i++)
+  {
+    if (bricks[i].is_alive)
+    {
       SDL_Rect brickRect = {(int)bricks[i].x, (int)bricks[i].y, bricks[i].width,
                             bricks[i].height};
       SDL_RenderFillRect(renderer, &brickRect);
@@ -251,11 +294,15 @@ void renderOutput(void) {
                       "    Lives: " + std::to_string(player.lives);
 
   // Check if font is valid
-  if (font != nullptr) {
+  if (font != nullptr)
+  {
     SDL_Surface *text = TTF_RenderText_Solid(font, score.c_str(), color);
-    if (!text) {
+    if (!text)
+    {
       std::cout << "Failed to render text: " << TTF_GetError() << std::endl;
-    } else {
+    }
+    else
+    {
       // Create texture
       SDL_Texture *text_texture;
 
@@ -282,7 +329,8 @@ void renderOutput(void) {
   SDL_RenderPresent(renderer);
 }
 
-void destroyWindow(void) {
+void destroyWindow(void)
+{
   TTF_CloseFont(font);
   TTF_Quit();
   SDL_DestroyRenderer(renderer);
@@ -290,12 +338,14 @@ void destroyWindow(void) {
   SDL_Quit();
 }
 
-int main(void) {
+int main(void)
+{
   game_is_running = initialiseWindow();
 
   setup();
 
-  while (game_is_running) {
+  while (game_is_running)
+  {
     processInput();
     updateGame();
     renderOutput();
