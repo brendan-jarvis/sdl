806dc2a46b4566f8442d921c37faa88c55b54445
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-09-13 19:12:15 +1200

    CMake setup

--- Asteroids/CMAKE/FindSDL2Mixer.cmake
+++ Asteroids/CMAKE/FindSDL2Mixer.cmake
@@ -1,100 +1,37 @@
-# Locate SDL_MIXER library
+# OpenClonk, http://www.openclonk.org
 #
-# This module defines:
+# Copyright (c) 2016, The OpenClonk Team and contributors
 #
-# ::
+# Distributed under the terms of the ISC license; see accompanying file
+# "COPYING" for details.
 #
-#   SDL2_MIXER_LIBRARIES, the name of the library to link against
-#   SDL2_MIXER_INCLUDE_DIRS, where to find the headers
-#   SDL2_MIXER_FOUND, if false, do not try to link against
-#   SDL2_MIXER_VERSION_STRING - human-readable string containing the version of SDL_MIXER
+# "Clonk" is a registered trademark of Matthes Bender, used with permission.
+# See accompanying file "TRADEMARK" for details.
 #
-#
-#
-# For backward compatibility the following variables are also set:
-#
-# ::
-#
-#   SDLMIXER_LIBRARY (same value as SDL2_MIXER_LIBRARIES)
-#   SDLMIXER_INCLUDE_DIR (same value as SDL2_MIXER_INCLUDE_DIRS)
-#   SDLMIXER_FOUND (same value as SDL2_MIXER_FOUND)
-#
-#
-#
-# $SDLDIR is an environment variable that would correspond to the
-# ./configure --prefix=$SDLDIR used in building SDL.
-#
-# Created by Eric Wing.  This was influenced by the FindSDL.cmake
-# module, but with modifications to recognize OS X frameworks and
-# additional Unix paths (FreeBSD, etc).
+# To redistribute this file separately, substitute the full license texts
+# for the above references.
 
-#=============================================================================
-# Copyright 2005-2009 Kitware, Inc.
-# Copyright 2012 Benjamin Eikel
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
+# Locate SDL_Mixer 2.
+# This module defines
+#  SDL2Mixer_INCLUDE_DIRS - a list of directories that need to be added to the include path
+#  SDL2Mixer_LIBRARIES - a list of libraries to link against to use SDL2
+#  SDL2Mixer_FOUND - if false, SDL2 cannot be used
 
-find_path(SDL2_MIXER_INCLUDE_DIR SDL_mixer.h
-        HINTS
-        ENV SDL2MIXERDIR
-        ENV SDL2DIR
-        PATH_SUFFIXES SDL2
-        # path suffixes to search inside ENV{SDLDIR}
-        include/SDL2 include
-        PATHS ${SDL2_MIXER_PATH}
-        )
-
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(VC_LIB_PATH_SUFFIX lib/x64)
-else()
-    set(VC_LIB_PATH_SUFFIX lib/x86)
+if(SDL2Mixer_FIND_QUIETLY)
+    set(_FIND_SDL2_ARG QUIET)
 endif()
 
-find_library(SDL2_MIXER_LIBRARY
-        NAMES SDL2_mixer
-        HINTS
-        ENV SDL2MIXERDIR
-        ENV SDL2DIR
-        PATH_SUFFIXES lib bin ${VC_LIB_PATH_SUFFIX}
-        PATHS ${SDL2_MIXER_PATH}
-        )
-
-if(SDL2_MIXER_INCLUDE_DIR AND EXISTS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_MIXER_MAJOR_VERSION[ \t]+[0-9]+$")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_MIXER_MINOR_VERSION[ \t]+[0-9]+$")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_MIXER_PATCHLEVEL[ \t]+[0-9]+$")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_MAJOR "${SDL2_MIXER_VERSION_MAJOR_LINE}")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_MINOR "${SDL2_MIXER_VERSION_MINOR_LINE}")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_PATCH "${SDL2_MIXER_VERSION_PATCH_LINE}")
-    set(SDL2_MIXER_VERSION_STRING ${SDL2_MIXER_VERSION_MAJOR}.${SDL2_MIXER_VERSION_MINOR}.${SDL2_MIXER_VERSION_PATCH})
-    unset(SDL2_MIXER_VERSION_MAJOR_LINE)
-    unset(SDL2_MIXER_VERSION_MINOR_LINE)
-    unset(SDL2_MIXER_VERSION_PATCH_LINE)
-    unset(SDL2_MIXER_VERSION_MAJOR)
-    unset(SDL2_MIXER_VERSION_MINOR)
-    unset(SDL2_MIXER_VERSION_PATCH)
-endif()
-
-set(SDL2_MIXER_LIBRARIES ${SDL2_MIXER_LIBRARY})
-set(SDL2_MIXER_INCLUDE_DIRS ${SDL2_MIXER_INCLUDE_DIR})
+find_package(SDL2 ${_FIND_SDL2_ARG})
+find_path(SDL2Mixer_INCLUDE_DIR SDL_mixer.h PATH_SUFFIXES SDL2 HINTS ENV SDL2DIR)
+mark_as_advanced(SDL2Mixer_INCLUDE_DIR)
+find_library(SDL2Mixer_LIBRARY SDL2_mixer HINTS ENV SDL2DIR)
+mark_as_advanced(SDL2Mixer_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SDL2Mixer REQUIRED_VARS SDL2Mixer_LIBRARY SDL2Mixer_INCLUDE_DIR
+        SDL2_LIBRARY SDL2_INCLUDE_DIR)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2_mixer
-        REQUIRED_VARS SDL2_MIXER_LIBRARIES SDL2_MIXER_INCLUDE_DIRS
-        VERSION_VAR SDL2_MIXER_VERSION_STRING)
-
-# for backward compatibility
-set(SDLMIXER_LIBRARY ${SDL2_MIXER_LIBRARIES})
-set(SDLMIXER_INCLUDE_DIR ${SDL2_MIXER_INCLUDE_DIRS})
-set(SDLMIXER_FOUND ${SDL2_MIXER_FOUND})
-
-mark_as_advanced(SDL2_MIXER_LIBRARY SDL2_MIXER_INCLUDE_DIR)
+if (SDL2Mixer_FOUND)
+    set(SDL2Mixer_LIBRARIES ${SDL2Mixer_LIBRARY} ${SDL2_LIBRARIES})
+    set(SDL2Mixer_INCLUDE_DIRS ${SDL2Mixer_INCLUDE_DIR} ${SDL2_INCLUDE_DIRS})
+endif()
---
+++ Asteroids/include/classes.h
@@ -0,0 +1,26 @@
+#pragma once 
+
+class Ball
+  {
+  public:
+    float x, y, direction;
+    int width, height, lives, score, speed, acceleration;
+    bool is_alive;
+  };
+
+class Player
+  {
+  public:
+    float x, y;
+    int width, height, lives, score, speed, acceleration;
+    bool is_alive;
+  };
+
+class Brick
+  {
+  public:
+    float x, y;
+    int width, height;
+    bool is_alive;
+  };
+
