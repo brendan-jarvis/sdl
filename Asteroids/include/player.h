e99909c7ecc6aec643891a43b2df4cd405834866
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-09-26 12:40:04 +1300

    Set up for animations

--- Asteroids/include/player.h
+++ Asteroids/include/player.h
@@ -1,15 +1,20 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include <vector>
 
 class Player {
 public:
   float centerX, centerY, acceleration, acceleration_X, acceleration_Y,
-      turnspeed, angle, friction, rotation;
+      turnspeed, angle, friction, rotation, boosterAngle;
   int lives, score, radius, size;
   SDL_Point linePoints[4];
-  bool isAlive, isAccelerating;
+  bool isAlive, isAccelerating, hasExploded;
+  SDL_Texture *explosionTexture, *boosterTexture;
+  SDL_Rect explosionRect, boosterRect, explosionDest, boosterDest;
+  std::vector<SDL_Rect> explosionFrames, boosterFrames;
 
-  Player();
+  Player(SDL_Renderer *renderer);
+  ~Player();
   void Reset(void);
   void Accelerate(void);
   void StopAccelerating(void);
